459c5248688c6f4c5eece878573c303dc1893782
Kristjan Vingel <kristjan.vingel@protonmail.ch>
2024-05-22 18:52:21 +0300

    Initial setup with Tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, CalendarComponent, HeaderComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'angular-calendar-app';
